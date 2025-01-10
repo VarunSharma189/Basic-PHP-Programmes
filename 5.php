b001b23437f9a7cb7e1c380c03e6554cb8dcdb83
Varun Sharma <sharmav1890@gmail.com>
2025-01-10 23:32:44 +0530

    Add files via upload

---
+++ 5.php
@@ -0,0 +1,10 @@
+WAP to demonstrate booelan data types in php
+<br>
+<?php
+
+    // Assign the value TRUE to a variable
+    $show_error = true;
+    var_dump($show_error);
+    echo "<br>";
+    echo "This Program is written by Varun Sharma 0221BCA136"    
+?>
